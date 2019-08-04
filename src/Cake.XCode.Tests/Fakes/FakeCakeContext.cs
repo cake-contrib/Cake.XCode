a73a847db67b9578112547315120fa2a6f67ffc1
Jonathan Dick <jondick@gmail.com>
2019-08-03 21:33:40 -0400

    Bump cake to 0.33

--- src/Cake.XCode.Tests/Fakes/FakeCakeContext.cs
+++ src/Cake.XCode.Tests/Fakes/FakeCakeContext.cs
@@ -22,13 +22,14 @@ namespace Cake.XCode.Tests.Fakes
             var globber = new Globber (fileSystem, environment);
             log = new FakeLog ();
             var args = new FakeCakeArguments ();
-            var processRunner = new ProcessRunner (environment, log);
             var registry = new WindowsRegistry ();
 			var toolRepo = new ToolRepository(environment);
 			var config = new Core.Configuration.CakeConfigurationProvider(fileSystem, environment).CreateConfiguration(testsDir, new Dictionary<string, string>());
 			var toolResolutionStrategy = new ToolResolutionStrategy(fileSystem, environment, globber, config);
 			var toolLocator = new ToolLocator(environment, toolRepo, toolResolutionStrategy);
-			context = new CakeContext(fileSystem, environment, globber, log, args, processRunner, registry, toolLocator);
+            var processRunner = new ProcessRunner(fileSystem, environment, log, toolLocator, config);
+            var dataService = new FakeDataService();
+            context = new CakeContext(fileSystem, environment, globber, log, args, processRunner, registry, toolLocator, dataService, config);
             context.Environment.WorkingDirectory = testsDir;
         }
 
---
+++ src/Cake.XCode.Tests/Fakes/FakeCakeDataService.cs
@@ -0,0 +1,20 @@
+﻿using Cake.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cake.XCode.Tests.Fakes
+{
+    public class FakeDataService : ICakeDataService
+    {
+        List<object> values = new List<object>();
+
+        public void Add<TData>(TData value) where TData : class
+        {
+            values.RemoveAll(v => v is TData);
+            values.Add(value);
+        }
+
+        public TData Get<TData>() where TData : class
+            => values.FirstOrDefault(v => v is TData) as TData;
+    }
+}
